c733c5986058f1332b5c00026271f67d61cf7a29
Clemens Lutz <lutzcle@cml.li>
2016-01-19 15:51:36 +0100

    Array-based CSV import for high-dimensional input

--- csv.hpp
+++ csv.hpp
@@ -8,6 +8,7 @@
 #include <cstring>
 #include <sstream>
 #include <cassert>
+#include <array>
 
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -17,48 +18,23 @@ namespace cle {
 
 class CSV {
 public:
-    template <typename ... Ts>
-    int read_csv(char const *file_name, Ts& ... vectors) {
+    template <typename T, typename ... Ts>
+    int read_csv(char const *file_name, std::vector<T>& vector, Ts& ... vectors) {
 
-        constexpr size_t NUM_VECTORS = sizeof...(Ts);
+        constexpr size_t NUM_VECTORS = sizeof...(Ts) + 1;
         CToken tokens[NUM_VECTORS];
-        struct stat file_stat;
-        int fd = 0;
         size_t file_size = 0;
-        int status = 0;
         char const * mapped = NULL;
         size_t file_offset = 0;
         size_t chars_tokenized = 0;
         std::stringstream ssbuf;
 
         // Open and mmap file
-        fd = open(file_name, O_RDONLY);
-        if (fd < 0) {
-            std::cerr << "Open file " << file_name << " failed with code "
-                << strerror(errno) << std::endl;
-        }
-
-        status = fstat(fd, &file_stat);
-        if (status < 0) {
-            std::cerr << "Stat file " << file_name << " failed with code "
-                << strerror(errno) << std::endl;
-        }
-        file_size = file_stat.st_size;
-
-        mapped = (char*) mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
-        if (mapped == MAP_FAILED) {
-            std::cerr << "Mmap file " << file_name << " failed with code "
-                << strerror(errno) << std::endl;
-        }
-
-        if(posix_madvise((void*)mapped, file_size, POSIX_MADV_WILLNEED) != 0) {
-            std::cerr << "Madvise failed with code "
-                << strerror(errno) << std::endl;
-        }
+        open_file(file_name, mapped, file_size);
 
         // Preallocate memory
         tune_vectors((file_size + NUM_VECTORS) / NUM_VECTORS,
-                vectors ...);
+                vector, vectors ...);
 
         // Process file
         file_offset = 0;
@@ -67,13 +43,43 @@ public:
                     NUM_VECTORS, delimiter_);
 
             if (chars_tokenized != 0) {
-                parse_line(ssbuf, tokens, vectors...);
+                parse_line(ssbuf, tokens, vector, vectors...);
             }
 
             file_offset += chars_tokenized;
         }
 
-        munmap((void*)mapped, file_size);
+        close_file(mapped, file_size);
+
+        return 1;
+    }
+
+    template <typename T, size_t size>
+    int read_csv(char const *file_name, std::array<std::vector<T>, size>& vectors) {
+
+        size_t file_size = 0;
+        char const * mapped = NULL;
+        size_t file_offset = 0;
+        size_t chars_tokenized = 0;
+        CToken tokens[size];
+        std::stringstream ssbuf;
+
+        open_file(file_name, mapped, file_size);
+
+        // Process file
+        file_offset = 0;
+        while (file_offset < file_size) {
+            chars_tokenized = tokenize(&mapped[file_offset], file_size, tokens,
+                    size, delimiter_);
+
+            if (chars_tokenized != 0) {
+                parse_line(ssbuf, tokens, vectors);
+            }
+
+            file_offset += chars_tokenized;
+        }
+
+        close_file(mapped, file_size);
 
         return 1;
     }
@@ -85,6 +91,54 @@ public:
 private:
     using Token = std::tuple<char*,size_t>;
     using CToken = std::tuple<char const*,size_t>;
+
+    int open_file(char const* file_name, char const*& mapped, size_t& file_size) {
+        struct stat file_stat = {};
+        int fd = 0;
+        int status = 0;
+
+        fd = open(file_name, O_RDONLY);
+        if (fd < 0) {
+            std::cerr << "Open file " << file_name << " failed with code "
+                << strerror(errno) << std::endl;
+            return fd;
+        }
+
+        status = fstat(fd, &file_stat);
+        if (status < 0) {
+            std::cerr << "Stat file " << file_name << " failed with code "
+                << strerror(errno) << std::endl;
+            return status;
+        }
+        file_size = file_stat.st_size;
+
+        mapped = (char*) mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+        if (mapped == MAP_FAILED) {
+            std::cerr << "Mmap file " << file_name << " failed with code "
+                << strerror(errno) << std::endl;
+            return errno;
+        }
+
+        if(posix_madvise((void*)mapped, file_size, POSIX_MADV_WILLNEED) != 0) {
+            std::cerr << "Madvise failed with code "
+                << strerror(errno) << std::endl;
+            return errno;
+        }
+
+        return 1;
+    }
+
+    int close_file(char const * const mapped, size_t const file_size) {
+        int ret = 0;
+
+        ret = munmap((void*)mapped, file_size);
+        if (ret < 0) {
+            std::cerr << "Munmap failed with code "
+                << strerror(errno) << std::endl;
+        }
+
+        return ret;
+    }
 
     size_t tokenize(char const *buffer, size_t buffer_size,
             CToken *tokens, size_t num_tokens, char delimiter) {
@@ -149,6 +203,23 @@ private:
         return 1;
     }
 
+    template <typename T, size_t size>
+    int parse_line(std::stringstream& ssbuf, CToken const *tokens,
+            std::array<std::vector<T>, size>& vectors) {
+
+        T v;
+
+        for (size_t i = 0; i != size; ++i) {
+            ssbuf.write(std::get<0>(tokens[i]), std::get<1>(tokens[i]));
+            ssbuf >> v;
+            vectors[i].push_back(v);
+            ssbuf.clear();
+            ssbuf.str("");
+        }
+
+        return 1;
+    }
+
     template <typename T>
     void tune_vectors(size_t avg_size, std::vector<T>& vec) {
         vec.reserve(avg_size / sizeof(T));
@@ -161,9 +232,17 @@ private:
         tune_vectors(avg_size, other ...);
     }
 
+    template <typename T, size_t size>
+    void tune_vectors(size_t avg_size, std::array<std::vector<T>, size>& vectors) {
+        for (std::vector<T>& v : vectors) {
+            tune_vectors(avg_size, v);
+        }
+
+    }
+
     char delimiter_ = ',';
 };
 
-};
+}
 
 #endif /* CSV_HPP */
--- test_csv.cpp
+++ test_csv.cpp
@@ -1,13 +1,18 @@
 #include "csv.hpp"
 
 #include "utils.hpp"
+#include "timer.hpp"
 
 #include <iostream>
 #include <vector>
+#include <cstdint>
+#include <chrono>
 
 int main(int argc, char **argv) {
     char *file_name = argv[1];
     cle::CSV csv;
+    cle::Timer timer;
+    uint64_t duration = 0;
 
     std::vector<double> a;
     std::vector<double> b;
@@ -16,9 +21,25 @@ int main(int argc, char **argv) {
 
     std::cout << "Reading from file \"" << file_name << "\"" << std::endl;
 
+    timer.start();
     csv.read_csv(file_name, a, b);
+    duration = timer.stop<std::chrono::microseconds>();
+
+    std::cout << "Variadic runtime: " << duration << " µs" << std::endl;
 
     // cle::Utils::print_vector(a);
     // cle::Utils::print_vector(b);
+
+    std::array<std::vector<double>, 2> array;
+
+    timer.start();
+    csv.read_csv(file_name, array);
+    duration = timer.stop<std::chrono::microseconds>();
+
+    std::cout << "Array runtime: " << duration << " µs" << std::endl;
+
+    // for (auto& v : array) {
+    //     cle::Utils::print_vector(v);
+    // }
 
 }
