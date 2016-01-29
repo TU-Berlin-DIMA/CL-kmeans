8354913d0464e4c28d62a5b5fa7cc344b7478487
Clemens Lutz <lutzcle@cml.li>
2016-01-29 14:42:26 +0100

    k-means using AVX2 SIMD instructions (aligned allocator doesn't work yet)

---
+++ kmeans_simd.cpp
@@ -0,0 +1,136 @@
+#include "kmeans.hpp"
+
+#include <cassert>
+#include <algorithm>
+#include <limits>
+#include <x86intrin.h>
+
+__m256 gaussian_distance_simd(
+        __m256 a_x, __m256 a_y,
+        __m256 b_x, __m256 b_y) {
+
+    __m256 t_x = _mm256_sub_ps(b_x, a_x);
+    __m256 t_y = _mm256_sub_ps(b_y, a_y);
+
+    t_x = _mm256_mul_ps(t_x, t_x);
+    t_y = _mm256_mul_ps(t_y, t_y);
+
+    return _mm256_add_ps(t_x, t_y);
+}
+
+float gaussian_distance(
+        float a_x, float a_y,
+        float b_x, float b_y
+        ) {
+
+    float t_x = b_x - a_x;
+    float t_y = b_y - a_y;
+
+    return t_x * t_x + t_y * t_y;
+}
+
+int cle::KmeansSIMD32::initialize() {
+    return 1;
+}
+
+int cle::KmeansSIMD32::finalize() {
+    return 1;
+}
+
+void cle::KmeansSIMD32::operator() (
+    uint32_t const max_iterations,
+    std::vector<float> const& points_x,
+    std::vector<float> const& points_y,
+    std::vector<float>& centroids_x,
+    std::vector<float>& centroids_y,
+    std::vector<uint32_t>& cluster_size,
+    std::vector<uint32_t>& memberships,
+    KmeansStats& stats) {
+
+    assert(points_x.size() == points_y.size());
+    assert(centroids_x.size() == centroids_y.size());
+    assert(memberships.size() == points_x.size());
+    assert(cluster_size.size() == centroids_x.size());
+
+    const uint32_t num_points = points_x.size();
+    const uint32_t num_clusters = centroids_x.size();
+    constexpr uint32_t vec_size = 8;
+
+    int did_changes = true;
+    uint32_t iterations = 0;
+    while (did_changes != 0 && iterations < max_iterations) {
+        did_changes = 0;
+
+        for (uint32_t p = 0; p < num_points; p += vec_size) {
+            __m256 min_distance;
+            __m256i min_centroid_id;
+
+            __m256 point_x = _mm256_load_ps(&points_x[p]);
+            __m256 point_y = _mm256_load_ps(&points_y[p]);
+
+            min_distance = _mm256_set1_ps(std::numeric_limits<float>::max());
+
+            for (uint32_t c = 0; c != num_clusters; ++c) {
+                __m256i centroid_id = _mm256_set1_epi32(c);
+                __m256 centroid_x = _mm256_set1_ps(centroids_x[c]);
+                __m256 centroid_y = _mm256_set1_ps(centroids_y[c]);
+
+                __m256 distance = gaussian_distance_simd(
+                        point_x, point_y,
+                        centroid_x, centroid_y
+                        );
+
+                __m256 min_mask = _mm256_cmp_ps(distance, min_distance, _CMP_LT_OS);
+                did_changes |= _mm256_movemask_ps(min_mask);
+                min_distance = _mm256_blendv_ps(min_distance, distance, min_mask);
+                min_centroid_id = _mm256_blendv_ps(min_centroid_id, centroid_id, min_mask);
+            }
+
+            _mm256_store_si256((__m256i*)&memberships[p], min_centroid_id);
+        }
+
+        uint32_t remain;
+        if ((remain = num_points % vec_size) != 0) {
+            for (uint32_t p = num_points - remain; p < num_points; ++p) {
+                float min_distance = std::numeric_limits<float>::max();
+                uint32_t min_centroid_id;
+
+                for (uint32_t c = 0; c != num_clusters; ++c) {
+                    float distance = gaussian_distance(
+                            points_x[p], points_y[p],
+                            centroids_x[c], centroids_y[c]
+                            );
+
+                    if (distance < min_distance) {
+                        min_distance = distance;
+                        min_centroid_id = c;
+                        did_changes |= 1;
+                    }
+                }
+
+                memberships[p] = min_centroid_id;
+            }
+        }
+
+        std::fill(cluster_size.begin(), cluster_size.end(), 0);
+        std::fill(centroids_x.begin(), centroids_x.end(), 0);
+        std::fill(centroids_y.begin(), centroids_y.end(), 0);
+
+        for (uint32_t p = 0; p != num_points; ++p) {
+            uint32_t c = memberships[p];
+
+            cluster_size[c] += 1;
+            centroids_x[c] += points_x[p];
+            centroids_y[c] += points_y[p];
+        }
+
+        for (uint32_t c = 0; c != num_clusters; ++c) {
+            centroids_x[c] = centroids_x[c] / cluster_size[c];
+            centroids_y[c] = centroids_y[c] / cluster_size[c];
+        }
+
+        ++iterations;
+    }
+
+    stats.iterations = iterations;
+}
--- test_kmeans.cpp
+++ test_kmeans.cpp
@@ -10,6 +10,8 @@
 #include <algorithm>
 #include <memory>
 
+#include <boost/align/aligned_allocator.hpp>
+
 int main(int argc, char **argv) {
 
   if (argc != 2) {
@@ -20,7 +22,9 @@ int main(int argc, char **argv) {
   char *input_path = argv[1];
 
   cle::CSV csv;
-  std::vector<double> points_x, points_y;
+  std::vector<float, boost::alignment::
+    aligned_allocator<float, 256>> points_x_32, points_y_32;
+  std::vector<double> points_x_64, points_y_64;
 
   cle::CLInitializer clinit;
   if (clinit.choose_platform_interactive() < 0) {
@@ -30,31 +34,52 @@ int main(int argc, char **argv) {
       return 0;
   }
 
+  csv.read_csv(input_path, points_x_32, points_y_32);
 
-  csv.read_csv(input_path, points_x, points_y);
+  csv.read_csv(input_path, points_x_64, points_y_64);
   std::cout << "Read " << input_path << std::endl;
-  uint64_t bytes_read = (points_x.size() + points_y.size()) * sizeof(double);
+  uint64_t bytes_read = (points_x_64.size() + points_y_64.size()) * sizeof(double);
   std::cout << "Read " << bytes_read / 1024 / 1024 << " MiB" << std::endl;
 
   constexpr uint32_t num_runs = 5;
   constexpr uint32_t max_iterations = 100;
   constexpr uint64_t num_clusters = 9;
-  uint64_t num_points = points_x.size();
+  uint64_t num_points = points_x_32.size();
+
+  cle::ClusteringBenchmark32 bm32(
+          num_runs, num_points, max_iterations,
+          std::move(points_x_32), std::move(points_y_32));
+  bm32.initialize(num_clusters, cle::KmeansInitializer32::first_x);
 
   cle::ClusteringBenchmark64 bm64(
           num_runs, num_points, max_iterations,
-          std::move(points_x), std::move(points_y));
-  bm64.initialize(num_clusters, cle::KmeansInitializer::first_x);
+          std::move(points_x_64), std::move(points_y_64));
+  bm64.initialize(num_clusters, cle::KmeansInitializer64::first_x);
+
+  cle::KmeansNaive32 kmeans_naive_32;
+  kmeans_naive_32.initialize();
 
   cle::KmeansNaive64 kmeans_naive_64;
   kmeans_naive_64.initialize();
+
+  cle::KmeansSIMD32 kmeans_simd_32;
+  kmeans_simd_32.initialize();
 
   cle::KmeansGPUAssisted kmeans_gpu_64(
           clinit.get_context(), clinit.get_commandqueue()
           );
   kmeans_gpu_64.initialize();
 
+  bm32.setVerificationReference(kmeans_naive_32);
   bm64.setVerificationReference(kmeans_naive_64);
+
+  int is_kmeans_simd_32_correct = bm32.verify(kmeans_simd_32);
+  if (is_kmeans_simd_32_correct) {
+      std::cout << "SIMD 32 is correct" << std::endl;
+  }
+  else {
+      std::cout << "SIMD 32 is wrong!!!" << std::endl;
+  }
 
   int is_kmeans_gpu_64_correct = bm64.verify(kmeans_gpu_64);
   if (is_kmeans_gpu_64_correct) {
@@ -67,7 +92,9 @@ int main(int argc, char **argv) {
   cle::ClusteringBenchmarkStats bs_naive_64 = bm64.run(kmeans_naive_64);
   cle::ClusteringBenchmarkStats bs_gpu_64 = bm64.run(kmeans_gpu_64);
 
+  kmeans_naive_32.finalize();
   kmeans_naive_64.finalize();
+  kmeans_simd_32.finalize();
   kmeans_gpu_64.finalize();
 
   bm64.finalize();
