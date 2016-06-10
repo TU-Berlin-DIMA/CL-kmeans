23af6ccc772ea2023857002fcc8ff2f6f11d7350
Clemens Lutz <lutzcle@cml.li>
2016-06-10 15:32:31 +0200

    Cleanup reduce vector test.

--- test_kernel.cpp
+++ test_kernel.cpp
@@ -30,7 +30,7 @@ namespace po = boost::program_options;
 
 class CmdOptions {
 public:
-    enum class Mode {GpuMemBandwidth, PciBandwidth};
+    enum class Mode {ReduceVector};
 
     int parse(int argc, char **argv) {
         char help_msg[] =
@@ -47,11 +47,15 @@ public:
             ("device",
              po::value<unsigned int>(&device_)->default_value(0),
              "OpenCL device number")
-            ("gpumembw", "GPU Memory Bandwidth")
-            ("pcibw", "PCI Bandwidth")
+            ("reducevector",
+             po::value<std::string>(&reduce_vector_),
+             "Reduce Vector")
             ("buffersize",
              po::value<size_t>(&buffer_size_)->default_value(256),
              "Buffer size in MiB")
+            ("rows",
+             po::value<uint32_t>(&rows_),
+             "Number of rows")
             ;
 
         po::variables_map vm;
@@ -73,16 +77,16 @@ public:
             device_ = vm["device"].as<unsigned int>();
         }
 
-        if (vm.count("gpumembw")) {
-            mode_ = Mode::GpuMemBandwidth;
-        }
-
-        if (vm.count("pcibw")) {
-            mode_ = Mode::PciBandwidth;
+        if (vm.count("reducevector")) {
+            mode_ = Mode::ReduceVector;
         }
 
         if (vm.count("buffersize")) {
             buffer_size_ = vm["buffersize"].as<size_t>();
+        }
+
+        if (vm.count("rows")) {
+            rows_ = vm["rows"].as<uint32_t>();
         }
 
         return 1;
@@ -100,15 +104,25 @@ public:
         return device_;
     }
 
+    std::string reduce_vector() const {
+        return reduce_vector_;
+    }
+
     size_t buffer_bytes() const {
         return buffer_size_ * 1024 * 1024;
+    }
+
+    uint32_t rows() const {
+        return rows_;
     }
 
 private:
     Mode mode_;
     unsigned int platform_;
     unsigned int device_;
+    std::string reduce_vector_;
     size_t buffer_size_;
+    uint32_t rows_;
 };
 
 class ReduceVectorParcolTest {
@@ -132,6 +146,96 @@ public:
     }
 };
 
+class ReduceVectorMode {
+public:
+    cl_int operator() () {
+        cle::Matrix<uint32_t, std::allocator<uint32_t>, uint32_t> data;
+        data.resize(num_rows_, num_cols_);
+
+        std::default_random_engine rgen;
+        std::uniform_int_distribution<uint32_t> uniform;
+        std::generate(data.begin(), data.end(), [&](){return uniform(rgen);});
+        // std::fill(data.begin(), data.end(), 1 << 31);
+        // std::generate(data.begin(), data.end(), [](){static uint32_t x = 0; return x++;});
+
+
+        std::vector<uint32_t> res_buffer(num_rows_);
+        cle::TypedBuffer<cl_uint> d_buffer(context_, CL_MEM_READ_WRITE, data.size());
+
+        cle::ReduceVectorParcolAPI<cl_uint, cl_uint> reducevector;
+        cle_sanitize_done_return(
+                reducevector.initialize(context_)
+                );
+
+        cle_sanitize_val_return(
+                queue_.enqueueWriteBuffer(
+                    d_buffer,
+                    CL_FALSE,
+                    0,
+                    d_buffer.bytes(),
+                    data.data(),
+                    NULL,
+                    NULL));
+
+        cl::Event event;
+        cle_sanitize_done_return(
+                reducevector(
+                    cl::EnqueueArgs(
+                        queue_,
+                        cl::NDRange(0),
+                        cl::NDRange(0)
+                        ),
+                    num_cols_,
+                    num_rows_,
+                    d_buffer,
+                    event));
+
+        cle_sanitize_val_return(
+                queue_.enqueueReadBuffer(
+                    d_buffer,
+                    CL_TRUE,
+                    0,
+                    res_buffer.size() * sizeof(uint32_t),
+                    res_buffer.data(),
+                    NULL,
+                    NULL));
+
+        ReduceVectorParcolTest reducetest;
+        if (reducetest(data, res_buffer)) {
+            std::cout << "Passed" << std::endl;
+        }
+        else {
+            std::cout << "Failed" << std::endl;
+        }
+
+        return CL_SUCCESS;
+    }
+
+    void set_context(cl::Context const& context) {
+        context_ = context;
+    }
+
+    void set_commandqueue(cl::CommandQueue const& queue) {
+        queue_ = queue;
+    }
+
+    void set_dimensions(uint32_t rows, uint32_t cols) {
+        num_rows_ = rows;
+        num_cols_ = cols;
+    }
+
+    void set_dimensions_by_size(size_t bytes, uint32_t rows) {
+        num_rows_ = rows;
+        num_cols_ = bytes / sizeof(uint32_t) / rows;
+    }
+
+private:
+        cl::Context context_;
+        cl::CommandQueue queue_;
+        uint32_t num_rows_;
+        uint32_t num_cols_;
+};
+
 int main(int argc, char **argv) {
 
     int ret = 0;
@@ -152,77 +256,15 @@ int main(int argc, char **argv) {
             );
 
     switch (options.get_mode()) {
-        case CmdOptions::Mode::GpuMemBandwidth:
-
+        case CmdOptions::Mode::ReduceVector:
+            ReduceVectorMode mode;
+            mode.set_context(initializer.get_context());
+            mode.set_commandqueue(initializer.get_commandqueue());
+            mode.set_dimensions_by_size(options.buffer_bytes(), options.rows());
+            mode();
             break;
-        case CmdOptions::Mode::PciBandwidth:
-
-            break;
     }
 
-    uint32_t num_rows = 64;
-    uint32_t num_cols = options.buffer_bytes() / sizeof(uint32_t) / num_rows;
-
-    cle::Matrix<uint32_t, std::allocator<uint32_t>, uint32_t> data;
-    data.resize(num_rows, num_cols);
-
-    std::default_random_engine rgen;
-    std::uniform_int_distribution<uint32_t> uniform;
-    std::generate(data.begin(), data.end(), [&](){return uniform(rgen);});
-    // std::fill(data.begin(), data.end(), 1 << 31);
-    // std::generate(data.begin(), data.end(), [](){static uint32_t x = 0; return x++;});
-
-    cl::Context context_ = initializer.get_context();
-    cl::CommandQueue queue_ = initializer.get_commandqueue();
-
-    std::vector<uint32_t> res_buffer(num_rows);
-    cle::TypedBuffer<cl_uint> d_buffer(context_, CL_MEM_READ_WRITE, data.size());
-
-    cle::ReduceVectorParcolAPI<cl_uint, cl_uint> reducevector;
-    cle_sanitize_done_return(
-            reducevector.initialize(context_)
-            );
-
-    cle_sanitize_val_return(
-            queue_.enqueueWriteBuffer(
-                d_buffer,
-                CL_FALSE,
-                0,
-                d_buffer.bytes(),
-                data.data(),
-                NULL,
-                NULL));
-
-    cl::Event event;
-    cle_sanitize_done_return(
-            reducevector(
-                cl::EnqueueArgs(
-                    queue_,
-                    cl::NDRange(0),
-                    cl::NDRange(0)
-                    ),
-                num_cols,
-                num_rows,
-                d_buffer,
-                event));
-
-    cle_sanitize_val_return(
-            queue_.enqueueReadBuffer(
-                d_buffer,
-                CL_TRUE,
-                0,
-                res_buffer.size() * sizeof(uint32_t),
-                res_buffer.data(),
-                NULL,
-                NULL));
-
-    ReduceVectorParcolTest reducetest;
-    if (reducetest(data, res_buffer)) {
-        std::cout << "Passed" << std::endl;
-    }
-    else {
-        std::cout << "Failed" << std::endl;
-    }
 
     return 0;
 }
