18d873dabe96a2efea18260f88b7d43bc83c202e
Clemens Lutz <lutzcle@cml.li>
2016-07-01 18:22:37 +0200

    Histogram with partition per work group using atomic_inc on global memory. Can be faster on certain HW according to Nvidia.

---
+++ cl_kernels/histogram_part_global.cl
@@ -0,0 +1,60 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * 
+ * Copyright (c) 2016, Lutz, Clemens <lutzcle@cml.li>
+ */
+
+#ifdef TYPE32
+#define CL_INT uint
+#else
+#ifdef TYPE64
+#define CL_INT ulong
+#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable
+#endif
+#endif
+
+/*
+ * Calculate histogram in paritions per work group
+ * 
+ * g_in: global_size
+ * g_out: num_work_groups * NUM_BINS
+ * l_bins: NUM_BINS
+ */
+__kernel
+void histogram_part_global(
+            __global CL_INT const *const restrict g_in,
+            __global CL_INT *const restrict g_out,
+            const CL_INT NUM_ITEMS,
+            const CL_INT NUM_BINS
+       ) {
+    CL_INT group_offset = get_group_id(0) * NUM_BINS;
+
+    for (
+            CL_INT r = get_global_id(0);
+            r < NUM_BINS * get_num_groups(0);
+            r += get_global_size(0)
+            ) {
+        g_bin[r] = 0;
+    }
+
+    barrier(CLK_GLOBAL_MEM_FENCE);
+
+    for (CL_INT r = 0; r < NUM_ITEMS; r += get_global_size(0)) {
+        // Current point ID
+        CL_INT p = r + get_global_id(0);
+
+        if (p < NUM_ITEMS) {
+            CL_INT bin_indx = g_in[p];
+#ifdef TYPE32
+            atomic_inc(&g_bins[group_offset + bin_indx]);
+#else
+#ifdef TYPE64
+            atom_inc(&g_bins[group_offset + bin_indx]);
+#endif
+#endif
+        }
+    }
+}
---
+++ cl_kernels/histogram_part_global_api.hpp
@@ -0,0 +1,117 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * 
+ * Copyright (c) 2016, Lutz, Clemens <lutzcle@cml.li>
+ */
+
+#ifndef HISTOGRAM_PART_GLOBAL_HPP
+#define HISTOGRAM_PART_GLOBAL_HPP
+
+#include "kernel_path.hpp"
+
+#include <clext.hpp>
+
+#include <functional>
+#include <cassert>
+#include <type_traits>
+#include <memory>
+
+#ifdef MAC
+#include <OpenCL/cl.hpp>
+#else
+#include <CL/cl.hpp>
+#endif
+
+namespace cle {
+
+    template <typename CL_INT>
+    class HistogramPartGlobalAPI {
+    public:
+        cl_int initialize(cl::Context& context) {
+            cl_int error_code = CL_SUCCESS;
+
+            std::string defines;
+            if (std::is_same<cl_uint, CL_INT>::value) {
+                defines = "-DTYPE32";
+            }
+            else if (std::is_same<cl_ulong, CL_INT>::value) {
+                defines = "-DTYPE64";
+            }
+            else {
+                assert(false);
+            }
+
+            cl::Program program = make_program(context, PROGRAM_FILE, defines, error_code);
+            if (error_code != CL_SUCCESS) {
+                return error_code;
+            }
+
+            mass_sum_kernel_.reset(new cl::Kernel(program, KERNEL_NAME, &error_code));
+            sanitize_make_kernel(error_code, context, program);
+
+            return error_code;
+        }
+
+        /*
+        * kernel HistogramPartLocal
+        * 
+        * Calculate histogram in partion per work group
+        * using global memory
+        *
+        * Input
+        *       Buffer with data item array
+        *
+        * Output
+        *       Buffer with #(work groups) partial histograms array
+        *
+        */
+        cl_int operator() (
+                cl::EnqueueArgs const& args,
+                CL_INT num_items,
+                CL_INT num_bins,
+                TypedBuffer<CL_INT>& in_items,
+                TypedBuffer<CL_INT>& out_bins,
+                cl::Event& event
+                ) {
+
+            assert(in_items.size() == num_items);
+            assert(out_bins.size() >=
+                    num_bins * (args.global_[0] / args.local_[0]));
+
+            cle_sanitize_val_return(
+                    mass_sum_kernel_->setArg(0, (cl::Buffer&)in_items));
+
+            cle_sanitize_val_return(
+                    mass_sum_kernel_->setArg(1, (cl::Buffer&)out_bins));
+
+            cle_sanitize_val_return(
+                    mass_sum_kernel_->setArg(2, num_items));
+
+            cle_sanitize_val_return(
+                    mass_sum_kernel_->setArg(3, num_bins));
+
+            cle_sanitize_val_return(
+                    args.queue_.enqueueNDRangeKernel(
+                    *mass_sum_kernel_,
+                    args.offset_,
+                    args.global_,
+                    args.local_,
+                    &args.events_,
+                    &event
+                    ));
+
+            return CL_SUCCESS;
+        }
+
+    private:
+        static constexpr const char* PROGRAM_FILE = CL_KERNEL_FILE_PATH("histogram_part_global.cl");
+        static constexpr const char* KERNEL_NAME = "histogram_part_global";
+
+        std::shared_ptr<cl::Kernel> mass_sum_kernel_;
+    };
+}
+
+#endif /* HISTOGRAM_PART_GLOBAL_HPP */
