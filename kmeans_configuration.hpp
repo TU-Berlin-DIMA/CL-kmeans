dc8c517eb4a654dfe51d044609507edb9219aa1b
Clemens Lutz <lutzcle@cml.li>
2016-11-30 11:50:35 +0100

    MPL 2.0 license text.

--- kmeans_configuration.hpp
+++ kmeans_configuration.hpp
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *
+ * Copyright (c) 2016, Lutz, Clemens <lutzcle@cml.li>
+ */
+
 #ifndef KMEANS_CONFIGURATION_HPP
 #define KMEANS_CONFIGURATION_HPP
 
