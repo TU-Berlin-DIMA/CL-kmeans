4b22fb6cdfcfc19eb2706ab1b4b1af8837a93b33
Clemens Lutz <lutzcle@cml.li>
2017-05-12 15:03:05 +0200

    Add license and copyright notice to vector_map_hack.hpp

--- container/vector_map_hack.hpp
+++ container/vector_map_hack.hpp
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *
+ * Copyright (c) 2017, Lutz, Clemens <lutzcle@cml.li>
+ */
+
 #ifndef VECTOR_MAP_HACK_HPP
 #define VECTOR_MAP_HACK_HPP
 
